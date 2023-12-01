81cf2c4a9fe85b0429b50dbd7c08699c44369510
Jose Wills <jose.wills3@gmail.com>
2023-12-01 21:48:13 +0000

    prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'airbnb',
-    'plugin:prettier/recommended',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "airbnb",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ const Title = () => {
     <div className="text-center">
       <h1 className="text-stone-200 text-6xl font-jost">Jose Wills</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
