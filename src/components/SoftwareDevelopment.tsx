6499d76249dd8e6e44d2b9a5c28f1ac9293ec804
Jose Wills <jose.wills3@gmail.com>
2023-12-10 09:38:12 +0000

    added project carousel

--- src/components/SoftwareDevelopment.tsx
+++ src/components/SoftwareDevelopment.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import Slider from "react-slick";
+import certags from "../assets/Certags artwork portal demo recording.mp4";
 
 const SoftwareDevelopment = () => {
   return (
-    <div>
-      <p>Software Development</p>
-    </div>
+    <section>
+      <Slider className="w-3/4 mx-auto">
+        <figure>
+          <video muted controls>
+            <source src={certags} />
+          </video>
+        </figure>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/9MXMxw37I-k?si=3Sg_0gfo6TQ6mAiH"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
+      </Slider>
+    </section>
   );
 };
 
