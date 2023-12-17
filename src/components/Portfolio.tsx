007c556af8aed9f2d61dd5f8b6d5ce8eb6d815d4
Jose Wills <jose.wills3@gmail.com>
2023-12-17 13:22:39 +0000

    added github repo link to new portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
   return (
     <article>
-      <h2 className="text-9xl text-center">This website!</h2>
+      <h2 className="text-9xl text-center mb-4">This website!</h2>
+      <Link
+        to="https://github.com/josenymad/new-portfolio"
+        className="block text-center underline"
+        target="_blank"
+      >
+        Check it out on GitHub
+      </Link>
     </article>
   );
 };
