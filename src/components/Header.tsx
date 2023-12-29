9a322fb015d59de3cd14fcc9bf6d12ad3bd71f9f
Jose Wills <jose.wills3@gmail.com>
2023-12-29 20:02:29 +0000

    header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import BurgerMenuNavbar from "./BurgerMenuNavbar.tsx";
 const Header = () => {
   return (
     <header className="text-center mb-8">
-      <h1 className="text-6xl mb-4">Jose Wills</h1>
+      <h1 className="text-5xl mb-4">Jose Wills</h1>
       {window.innerWidth > 750 ? <Navbar /> : <BurgerMenuNavbar />}
     </header>
   );
