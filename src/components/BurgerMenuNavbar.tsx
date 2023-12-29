b926384b883258d5325e712507b9009797e7c328
Jose Wills <jose.wills3@gmail.com>
2023-12-29 18:40:12 +0000

    styling for burger menu sidebar

--- src/components/BurgerMenuNavbar.tsx
+++ src/components/BurgerMenuNavbar.tsx
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 
 const BurgerMenuNavbar = () => {
   return (
-    <Menu>
+    <Menu className="fixed left-0">
       <Link to="/">About</Link>
+      <span>⌯</span>
       <Link to="software-development">Software Development</Link>
+      <span>⌯</span>
       <Link to="music-production">Music Production</Link>
+      <span>⌯</span>
       <Link to="contact">Contact</Link>
     </Menu>
   );
