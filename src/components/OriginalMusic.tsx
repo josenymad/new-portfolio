f3f5be420796c00e8ddda5cf85823d48089f3807
Jose Wills <jose.wills3@gmail.com>
2023-12-30 17:47:51 +0000

    added shadow to iframes of first slide in music production

--- src/components/OriginalMusic.tsx
+++ src/components/OriginalMusic.tsx
@@ -4,7 +4,7 @@ const OriginalMusic = () => {
   return (
     <section className="text-center grid grid-rows-4 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 2xl:grid-cols-4 2xl:grid-rows-1 gap-y-4">
       <iframe
-        className="h-[515px] mx-auto"
+        className="h-[515px] mx-auto shadow-2xl"
         src="https://bandcamp.com/EmbeddedPlayer/album=2812127603/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/transparent=true/"
         seamless
         title="Bandcamp Music Player"
@@ -14,7 +14,7 @@ const OriginalMusic = () => {
         </a>
       </iframe>
       <iframe
-        className="h-[515px] mx-auto"
+        className="h-[515px] mx-auto shadow-2xl"
         src="https://bandcamp.com/EmbeddedPlayer/album=3771502757/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/transparent=true/"
         seamless
         title="Bandcamp Music Player"
@@ -24,7 +24,7 @@ const OriginalMusic = () => {
         </a>
       </iframe>
       <iframe
-        className="h-[515px] mx-auto"
+        className="h-[515px] mx-auto shadow-2xl"
         src="https://bandcamp.com/EmbeddedPlayer/album=2070245364/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/transparent=true/"
         seamless
         title="Bandcamp Music Player"
@@ -34,7 +34,7 @@ const OriginalMusic = () => {
         </a>
       </iframe>
       <iframe
-        className="h-[515px] mx-auto lg:max-2xl:col-span-3"
+        className="h-[515px] mx-auto shadow-2xl lg:max-2xl:col-span-3"
         src="https://bandcamp.com/EmbeddedPlayer/album=2600371896/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/transparent=true/"
         seamless
         title="Bandcamp Music Player"
